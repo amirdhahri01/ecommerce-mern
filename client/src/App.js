5a994f17089a8ac715584c9b91539ba8d7b2f723
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-22 09:39:36 +0100

    An update

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
 import Navbar from './components/Navbar';
 import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Route, Routes as Router } from "react-router-dom"
+import ProductDescScreen from "./screens/ProductDescScreen";
 
 function App() {
   return (
-  
-    <div className="App">
-        <Navbar/> 
-        <HomeScreen/> 
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Router>
+        <Route path="/" Component={HomeScreen} />
+        <Route path="/product/:id" Component={ProductDescScreen} />
+      </Router>
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/screens/ProductDescScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductDescScreen = () => {
+    return (
+        <div>ProductDescScreen</div>
+    )
+}
+
+export default ProductDescScreen
