22835e7231d4c3947232b58f0670313607de96e5
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-22 13:17:55 +0100

    An update

--- client/src/store.js
+++ client/src/store.js
@@ -1,9 +1,15 @@
 import { getAllProductsReducer } from "./reducers/productReducer"
 import { createStore, applyMiddleware, combineReducers } from "redux"
 import thunk from "redux-thunk"
-
+import { composeWithDevTools} from "redux-devtools-extension"
 const finalReducer = combineReducers({
     getAllProductsReducer: getAllProductsReducer
 })
+const composeEnhancers = composeWithDevTools({
 
-const store = createStore([finalReducer, applyMiddleware(thunk)])
+}) 
+const store = createStore([finalReducer,  composeEnhancers(
+    applyMiddleware(thunk)
+)])
+
+export default store
