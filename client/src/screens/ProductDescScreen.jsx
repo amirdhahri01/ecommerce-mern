8d14dbabecc549e8fd4d090e8fbc1a33a3057cd5
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-22 09:48:24 +0100

    An update

--- client/src/screens/ProductDescScreen.jsx
+++ client/src/screens/ProductDescScreen.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import products from '../products'
 
-const ProductDescScreen = ({match}) => {
+const ProductDescScreen = ({ match }) => {
     const productID = match.params.id;
+    const product = products.find((product) => product.id === productID);
     return (
-        <div>ProductDescScreen</div>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <div className="card">
+                        <h1>{product.name}</h1>
+                        <img src={product.image} alt="" className="img-fluid" />
+                        <p>{product.description}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
