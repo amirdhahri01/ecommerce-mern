87b67244979c6cf1aec62581907874cad509f74a
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-22 13:19:43 +0100

    An update

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react'
 import Product from '../components/Product'
+import { getALLProducts } from '../actions/productActions'
+import { useDispatch } from 'react-redux'
 const HomeScreen = () => {
     const [products, setProducts] = useState([])
+    const dispatch = useDispatch();
     useEffect(() => {
-        
+        dispatch(getALLProducts);
     }, [])
     return (
         <div className="container-fluid">
             <div className="row justify-content-center">
                 {products.length && products.map((product) => (
-                    <Product product={product}  key={product._id}/>
+                    <Product product={product} key={product._id} />
                 ))}
             </div>
         </div>
