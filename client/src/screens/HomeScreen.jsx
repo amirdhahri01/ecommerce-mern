282276b79135b0723ed3a668090e043fcc597053
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-22 02:30:27 +0100

    An update

--- client/src/screens/HomeScreen.jsx
+++ client/src/screens/HomeScreen.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-
+import products from '../products'
+import Produrct from '../components/Product'
 const HomeScreen = () => {
-  return (
-    <div>HomeScreen</div>
-  )
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                {products.map((product, id) => (
+                    <Produrct product={product} key={id} />
+                ))}
+            </div>
+        </div>
+    )
 }
-
 export default HomeScreen
