c8f01029934d25800bc159f78ddbe440179d5a40
Amir Dhahri <amirdhahri01@gmail.com>
2024-12-22 01:57:06 +0100

    An update

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+        <Navbar/> 
     </div>
   );
 }
